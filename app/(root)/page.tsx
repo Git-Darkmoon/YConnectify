430931f2e24c193ceb449d849d53740cedc61c95
Nicolas Mayorga <namayorgav@gmail.com>
2025-01-07 17:17:23 -0500

    feat: add all startups section and startup card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@components/StartupCard"
 import SearchBar from "@components/SearchBar"
 
 const Home = async ({
@@ -6,6 +7,21 @@ const Home = async ({
   searchParams: Promise<{ query?: string }>
 }) => {
   const { query } = await searchParams
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Pepe el Grillo" },
+      _id: "1",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+      image:
+        "https://150785984.v2.pressablecdn.com/wp-content/uploads/2025/01/nvidia-geforce-50-series.webp",
+      category: "Technology",
+      title: "Nvidia 5000 series released",
+    },
+  ]
 
   return (
     <>
@@ -17,6 +33,22 @@ const Home = async ({
           Submit ideas, vote on pitches, and get noticed in virtual
         </p>
         <SearchBar query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => {
+              return <StartupCard key={post?._id} post={post} />
+            })
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   )
